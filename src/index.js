525accba1445448dfedaf5a74ca7e9bea716e252
chris <chrisivey01@gmail.com>
2020-11-30 09:58:00 -0600

    turn off logger for main

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from "redux-thunk";
 import App from './App';
 import combineReducers from "./store/index";
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
-const middle = [thunk, logger]
+const middle = [thunk]
 const store = createStore(combineReducers, applyMiddleware(...middle));
 
 ReactDOM.render(
